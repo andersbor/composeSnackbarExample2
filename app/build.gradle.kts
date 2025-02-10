b56ae63b23bf163206d968c02f76f67a0be3bf3f
andersbor <anbo@zealand.dk>
2025-02-10 12:56:57 +0100

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.snackbarexample2"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.snackbarexample2"
--- app/src/main/java/com/example/snackbarexample2/MainActivity.kt
+++ app/src/main/java/com/example/snackbarexample2/MainActivity.kt
@@ -5,7 +5,9 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -21,8 +23,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.snackbarexample2.ui.theme.SnackbarExample2Theme
 import kotlinx.coroutines.launch
 
@@ -30,55 +34,61 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContent {
             SnackbarExample2Theme {
-                ScaffoldWithSnackbars()
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun ScaffoldWithSnackbars() {
+fun MainScreen() {
     val scope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
     var message by remember { mutableStateOf("") }
     Scaffold(
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
     ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.Top)
+        ) {
             Button(modifier = Modifier.fillMaxWidth(),
                 onClick = {
-                scope.launch {
-                    snackbarHostState.showSnackbar("Simple Snackbar!")
-                }
-            }) {
-                Text("Show simple Snackbar")
+                    scope.launch {
+                        snackbarHostState.showSnackbar("Simple Snackbar!")
+                    }
+                }) {
+                Text("Show Simple Snackbar")
             }
             Button(modifier = Modifier.fillMaxWidth(),
                 onClick = {
-                scope.launch {
-                    val result: SnackbarResult = snackbarHostState
-                        .showSnackbar(
-                            message = "Snackbar with action",
-                            actionLabel = "Action",
-                            withDismissAction = true,
-                            duration = SnackbarDuration.Long
-                        )
-                    when (result) {
-                        SnackbarResult.ActionPerformed -> {
-                            Log.d("Snackbar", "Action performed")
-                            message = "Action performed"
-                        }
-                        SnackbarResult.Dismissed -> {
-                            Log.d("Snackbar", "Dismissed")
-                            message = "Dismissed"
+                    scope.launch {
+                        val result: SnackbarResult = snackbarHostState
+                            .showSnackbar(
+                                message = "Snackbar with action",
+                                actionLabel = "Action",
+                                withDismissAction = true,
+                                duration = SnackbarDuration.Long
+                            )
+                        when (result) {
+                            SnackbarResult.ActionPerformed -> {
+                                Log.d("Snackbar", "Action performed")
+                                message = "Action performed"
+                            }
+
+                            SnackbarResult.Dismissed -> {
+                                Log.d("Snackbar", "Dismissed")
+                                message = "Dismissed"
+                            }
                         }
                     }
-                }
-            }) {
-                Text("Show Snackbar with action")
+                }) {
+                Text("Show Snackbar With Action")
             }
             Text(message)
         }
@@ -89,6 +99,6 @@ fun ScaffoldWithSnackbars() {
 @Composable
 fun ScaffoldWithSnackbarsPreview() {
     SnackbarExample2Theme {
-        ScaffoldWithSnackbars()
+        MainScreen()
     }
 }
